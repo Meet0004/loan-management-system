4c14ba921ef9ee7a1c0090395051531cff2c672b
Meet Soni <113117130+Meet0004@users.noreply.github.com>
2025-08-28 22:23:24 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,12 +2,8 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://meetgssoni04:C3Qz7d2LesdFQJfW@lms.fqkiiiu.mongodb.net/loanDB?retryWrites=true&w=majority', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+        await mongoose.connect('mongodb+srv://meetgssoni04:C3Qz7d2LesdFQJfW@lms.fqkiiiu.mongodb.net/loanDB?retryWrites=true&w=majority');
         console.log('Connected to MongoDB');
-
         console.log('ab kaam karsakte hai');
     } catch (err) {
         console.error('Failed to connect to MongoDB:', err);

86aa04931971322dd634e67ecdc86ea6cc8caa6c
Meet Soni <113117130+Meet0004@users.noreply.github.com>
2025-08-28 20:15:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,18 +16,18 @@ try {
 // Middleware
 app.use(express.json()); // Middleware to parse JSON
 app.use(express.urlencoded({ extended: false })); // Middleware to parse URL-encoded data
-app.use("/public", express.static(path.join(__dirname, "static")));
+app.use(express.static(path.join(__dirname, "static")));
 
 // Routes
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "static", "index.html"));
 });
 
-app.get("https://loan-management-system-vsy1.onrender.com/login", (req, res) => {
+app.get("/login", (req, res) => {
     res.sendFile(path.join(__dirname, "static", "login.html"));
 });
 
-app.post("https://loan-management-system-vsy1.onrender.com/save-data", async (req, res) => {
+app.post("/save-data", async (req, res) => {
     try {
         const {
             name,
@@ -127,7 +127,7 @@ const fetchUserData = async (username) => {
 
 //fetchUserData("Meet-Soni");
 
-app.get('https://loan-management-system-vsy1.onrender.com/user/:username', async (req, res) => {
+app.get('/user/:username', async (req, res) => {
     const username = req.params.username;
     try {
         const userData = await User.findOne({ Customer: username });
@@ -152,7 +152,7 @@ app.get('https://loan-management-system-vsy1.onrender.com/user/:username', async
     }
 });
 
-app.post('https://loan-management-system-vsy1.onrender.com/send-data', (req, res) => {
+app.post('/send-data', (req, res) => {
     // destructure the correct keys from req.body
     const { usernameTOBE, passwordTOBE } = req.body;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
 
@@ -167,13 +167,13 @@ app.post('https://loan-management-system-vsy1.onrender.com/send-data', (req, res
     res.send('Data received and separated successfully!');
 });
 
-app.get("https://loan-management-system-vsy1.onrender.com/check-username/:username", async (req, res) => {
+app.get("/check-username/:username", async (req, res) => {
     const { username } = req.params;
     const user = await RegisterUser.findOne({ Customer: username });
     res.json({ exists: !!user });
 });
 
-app.post("https://loan-management-system-vsy1.onrender.com/check-user", async (req, res) => {
+app.post("/check-user", async (req, res) => {
     try {
         const { username, password } = req.body;
         const user = await RegisterUser.findOne({ Customer: username });
@@ -189,7 +189,7 @@ app.post("https://loan-management-system-vsy1.onrender.com/check-user", async (r
     }
 });
 
-app.post("https://loan-management-system-vsy1.onrender.com/register", async (req, res) => {
+app.post("/register", async (req, res) => {
     try {
         const { username, password } = req.body;
 
@@ -213,12 +213,9 @@ app.post("https://loan-management-system-vsy1.onrender.com/register", async (req
         res.status(500).json({ message: "Server error, try again!" });
     }
 });
-
-console.log("https://loan-management-system-vsy1.onrender.com")
-
-const PORT = 7700;
+const PORT = process.env.PORT || 7700;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
-//.
+
 //module.exports = { fetchUserData };
